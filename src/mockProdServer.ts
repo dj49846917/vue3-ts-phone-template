5bcee4b5fc50672d175eb2c23b871e543c238087
dj49846917 <821084785@qq.com>
2021-11-25 17:20:32 +0800

    '发布分支'

--- src/mockProdServer.ts
+++
@@ -1,8 +0,0 @@
-//  mockProdServer.ts
-import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
-
-// 逐一导入您的mock.ts文件
-import userApi from '../mock/auction'
-export function setupProdMockServer() {
-  createProdMockServer([...userApi]);
-}
--- src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { createStore } from "vuex";
-import user from '@/store/modules/user'
-export default createStore({
-  modules: {
-    user
-  }
-});
